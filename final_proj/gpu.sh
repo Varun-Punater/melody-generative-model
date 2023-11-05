26bd830ae3c73075a7175868116a97cf59394de9
Varun Punater <punater@usc.edu>
2023-11-05 14:09:19 -0800

    adding gpu checks

---
+++ final_proj/gpu.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --account=robinjia_1152
+#SBATCH --partition=gpu
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --gpus-per-task=a40:1
+#SBATCH --mem=16G
+#SBATCH --time=1:00:00
+
+module purge
+module load gcc/11.3.0
+module load python/3.9.12
+module spider cuda
+module spider cudnn
+module load nvhpc/22.11
+eval "$(conda shell.bash hook)"
+conda activate 467
+
+python gpu_check.py
