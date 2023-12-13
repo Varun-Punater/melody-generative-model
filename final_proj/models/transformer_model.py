aa35c48751e9a1fba5abfe9a9cc8dbd379f15053
Rijul Raghu <jraghu@discovery1.usc.edu>
2023-12-12 19:57:44 -0800

    final transformer setup

--- final_proj/models/transformer_model.py
+++ final_proj/models/transformer_model.py
@@ -2,7 +2,6 @@ import math
 import os
 from tempfile import TemporaryDirectory
 from typing import Tuple
-
 import torch
 from torch import nn, Tensor
 from torch.nn import TransformerEncoder, TransformerEncoderLayer
